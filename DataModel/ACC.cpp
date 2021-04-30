16f3d3c53f0cd0e6994dcffa50133ada5c9abc97
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-04-30 15:48:47 +0200

    Update ACC.cpp

--- DataModel/ACC.cpp
+++ DataModel/ACC.cpp
@@ -416,6 +416,8 @@ int ACC::readAcdcBuffers()
 			if(lastAccBuffer.at(16+k)==7795)
 			{
 				boardsReadyForRead.push_back(k);
+			}else if(lastAccBuffer.at(16+k)!=0 && lastAccBuffer.at(16+k)!=7795) {
+				std::cout << "Board " << k << " has buffer " << lastAccBuffer.at(16+k);
 			}
 		}
 
