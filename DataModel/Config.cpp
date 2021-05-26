16aa7b28fa8bbcf6e389ad22a7562dfa4c75ce79
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-26 15:17:01 +0200

    Update Config.cpp

--- DataModel/Config.cpp
+++ DataModel/Config.cpp
@@ -10,83 +10,80 @@ bool Config::Send(zmq::socket_t* sock){
   zmq::message_t msg2(sizeof triggermode);
   memcpy(msg2.data(), &triggermode, sizeof triggermode);
 
-  zmq::message_t msg3(sizeof ACC_Mode);
-  memcpy(msg3.data(), &ACC_Mode, sizeof ACC_Mode);
+  zmq::message_t msg3(sizeof ACC_Sign);
+  memcpy(msg3.data(), &ACC_Sign, sizeof ACC_Sign);
 
-  zmq::message_t msg4(sizeof ACC_Sign);
-  memcpy(msg4.data(), &ACC_Sign, sizeof ACC_Sign);
+  zmq::message_t msg4(sizeof ACDC_Sign);
+  memcpy(msg4.data(), &ACDC_Sign, sizeof ACDC_Sign);      
 
-  zmq::message_t msg5(sizeof ACDC_Mode);
-  memcpy(msg5.data(), &ACDC_Mode, sizeof ACDC_Mode);
+  zmq::message_t msg5(sizeof SELF_Sign);
+  memcpy(msg5.data(), &SELF_Sign, sizeof SELF_Sign);
 
-  zmq::message_t msg6(sizeof ACDC_Sign);
-  memcpy(msg6.data(), &ACDC_Sign, sizeof ACDC_Sign);      
+  zmq::message_t msg6(sizeof SELF_Enable_Coincidence);
+  memcpy(msg6.data(), &SELF_Enable_Coincidence, sizeof SELF_Enable_Coincidence);
 
-  zmq::message_t msg7(sizeof SELF_Mode);
-  memcpy(msg7.data(), &SELF_Mode, sizeof SELF_Mode);
+  zmq::message_t msg7(sizeof SELF_Coincidence_Number);
+  memcpy(msg7.data(), &SELF_Coincidence_Number, sizeof SELF_Coincidence_Number);
 
-  zmq::message_t msg8(sizeof SELF_Sign);
-  memcpy(msg8.data(), &SELF_Sign, sizeof SELF_Sign);
+  zmq::message_t msg8(sizeof SELF_threshold);
+  memcpy(msg8.data(), &SELF_threshold, sizeof SELF_threshold);
 
-  zmq::message_t msg9(sizeof SELF_Enable_Coincidence);
-  memcpy(msg9.data(), &SELF_Enable_Coincidence, sizeof SELF_Enable_Coincidence);
+  zmq::message_t msg9(sizeof ACDC_mask);
+  memcpy(msg9.data(), &ACDC_mask, sizeof ACDC_mask); 
 
-  zmq::message_t msg10(sizeof SELF_Coincidence_Number);
-  memcpy(msg10.data(), &SELF_Coincidence_Number, sizeof SELF_Coincidence_Number);
+  zmq::message_t msg10(sizeof PSEC_Chip_Mask_0);
+  memcpy(msg10.data(), &PSEC_Chip_Mask_0, sizeof PSEC_Chip_Mask_0);
 
-  zmq::message_t msg11(sizeof SELF_threshold);
-  memcpy(msg11.data(), &SELF_threshold, sizeof SELF_threshold);
+  zmq::message_t msg11(sizeof PSEC_Chip_Mask_1);
+  memcpy(msg11.data(), &PSEC_Chip_Mask_1, sizeof PSEC_Chip_Mask_1);
 
-  zmq::message_t msg12(sizeof ACDC_mask);
-  memcpy(msg12.data(), &ACDC_mask, sizeof ACDC_mask); 
+  zmq::message_t msg12(sizeof PSEC_Chip_Mask_2);
+  memcpy(msg12.data(), &PSEC_Chip_Mask_2, sizeof PSEC_Chip_Mask_2);
 
-  zmq::message_t msg13(sizeof PSEC_Chip_Mask_0);
-  memcpy(msg13.data(), &PSEC_Chip_Mask_0, sizeof PSEC_Chip_Mask_0);
+  zmq::message_t msg13(sizeof PSEC_Chip_Mask_3);
+  memcpy(msg13.data(), &PSEC_Chip_Mask_3, sizeof PSEC_Chip_Mask_3);
 
-  zmq::message_t msg14(sizeof PSEC_Chip_Mask_1);
-  memcpy(msg14.data(), &PSEC_Chip_Mask_1, sizeof PSEC_Chip_Mask_1);
+  zmq::message_t msg14(sizeof PSEC_Chip_Mask_4);
+  memcpy(msg14.data(), &PSEC_Chip_Mask_4, sizeof PSEC_Chip_Mask_4);
 
-  zmq::message_t msg15(sizeof PSEC_Chip_Mask_2);
-  memcpy(msg15.data(), &PSEC_Chip_Mask_2, sizeof PSEC_Chip_Mask_2);
+  zmq::message_t msg15(sizeof PSEC_Channel_Mask_0);
+  memcpy(msg15.data(), &PSEC_Channel_Mask_0, sizeof PSEC_Channel_Mask_0);      
 
-  zmq::message_t msg16(sizeof PSEC_Chip_Mask_3);
-  memcpy(msg16.data(), &PSEC_Chip_Mask_3, sizeof PSEC_Chip_Mask_3);
+  zmq::message_t msg16(sizeof PSEC_Channel_Mask_1);
+  memcpy(msg16.data(), &PSEC_Channel_Mask_1, sizeof PSEC_Channel_Mask_1);
 
-  zmq::message_t msg17(sizeof PSEC_Chip_Mask_4);
-  memcpy(msg17.data(), &PSEC_Chip_Mask_4, sizeof PSEC_Chip_Mask_4);
+  zmq::message_t msg17(sizeof PSEC_Channel_Mask_2);
+  memcpy(msg17.data(), &PSEC_Channel_Mask_2, sizeof PSEC_Channel_Mask_2);
 
-  zmq::message_t msg18(sizeof PSEC_Channel_Mask_0);
-  memcpy(msg18.data(), &PSEC_Channel_Mask_0, sizeof PSEC_Channel_Mask_0);      
+  zmq::message_t msg18(sizeof PSEC_Channel_Mask_3);
+  memcpy(msg18.data(), &PSEC_Channel_Mask_3, sizeof PSEC_Channel_Mask_3);
 
-  zmq::message_t msg19(sizeof PSEC_Channel_Mask_1);
-  memcpy(msg19.data(), &PSEC_Channel_Mask_1, sizeof PSEC_Channel_Mask_1);
+  zmq::message_t msg19(sizeof PSEC_Channel_Mask_4);
+  memcpy(msg19.data(), &PSEC_Channel_Mask_4, sizeof PSEC_Channel_Mask_4);
 
-  zmq::message_t msg20(sizeof PSEC_Channel_Mask_2);
-  memcpy(msg20.data(), &PSEC_Channel_Mask_2, sizeof PSEC_Channel_Mask_2);
-
-  zmq::message_t msg21(sizeof PSEC_Channel_Mask_3);
-  memcpy(msg21.data(), &PSEC_Channel_Mask_3, sizeof PSEC_Channel_Mask_3);
-
-  zmq::message_t msg22(sizeof PSEC_Channel_Mask_4);
-  memcpy(msg22.data(), &PSEC_Channel_Mask_4, sizeof PSEC_Channel_Mask_4);
-
-  zmq::message_t msg23(sizeof Validation_Start);
-  memcpy(msg23.data(), &Validation_Start, sizeof Validation_Start);
+  zmq::message_t msg20(sizeof Validation_Start);
+  memcpy(msg20.data(), &Validation_Start, sizeof Validation_Start);
 	
-  zmq::message_t msg24(sizeof Validation_Window);
-  memcpy(msg24.data(), &Validation_Window, sizeof Validation_Window);
+  zmq::message_t msg21(sizeof Validation_Window);
+  memcpy(msg21.data(), &Validation_Window, sizeof Validation_Window);
 
-  zmq::message_t msg25(sizeof Calibration_Mode);
-  memcpy(msg25.data(), &Calibration_Mode, sizeof Calibration_Mode); 
+  zmq::message_t msg22(sizeof Calibration_Mode);
+  memcpy(msg22.data(), &Calibration_Mode, sizeof Calibration_Mode); 
 
-  zmq::message_t msg26(sizeof Raw_Mode);
-  memcpy(msg26.data(), &Raw_Mode, sizeof Raw_Mode);
+  zmq::message_t msg23(sizeof Raw_Mode);
+  memcpy(msg23.data(), &Raw_Mode, sizeof Raw_Mode);
 
-  zmq::message_t msg27(sizeof Pedestal_channel);
-  memcpy(msg27.data(), &Pedestal_channel, sizeof Pedestal_channel);
+  zmq::message_t msg24(sizeof Pedestal_channel);
+  memcpy(msg24.data(), &Pedestal_channel, sizeof Pedestal_channel);
 
-  zmq::message_t msg28(sizeof Pedestal_channel_mask);
-  memcpy(msg28.data(), &Pedestal_channel_mask, sizeof Pedestal_channel_mask); 
+  zmq::message_t msg25(sizeof Pedestal_channel_mask);
+  memcpy(msg25.data(), &Pedestal_channel_mask, sizeof Pedestal_channel_mask); 
+	
+  zmq::message_t msg26(sizeof PPSRatio);
+  memcpy(msg26.data(), &PPSRatio, sizeof PPSRatio); 
+	
+  zmq::message_t msg27(sizeof PPSBeamMultiplexer);
+  memcpy(msg27.data(), &PPSBeamMultiplexer, sizeof PPSBeamMultiplexer); 
 
   sock->send(msg1,ZMQ_SNDMORE);
   sock->send(msg2,ZMQ_SNDMORE);
@@ -114,8 +111,7 @@ bool Config::Send(zmq::socket_t* sock){
   sock->send(msg24,ZMQ_SNDMORE);
   sock->send(msg25,ZMQ_SNDMORE);
   sock->send(msg26,ZMQ_SNDMORE);
-  sock->send(msg27,ZMQ_SNDMORE);
-  sock->send(msg28);
+  sock->send(msg27);
 	
   return true;
 }
@@ -132,15 +128,9 @@ bool Config::Receive(zmq::socket_t* sock){
   sock->recv(&msg);
   triggermode=*(reinterpret_cast<int*>(msg.data()));
   sock->recv(&msg);
-  ACC_Mode=*(reinterpret_cast<int*>(msg.data()));
-  sock->recv(&msg);
   ACC_Sign=*(reinterpret_cast<int*>(msg.data()));
   sock->recv(&msg);
-  ACDC_Mode=*(reinterpret_cast<int*>(msg.data()));
-  sock->recv(&msg);
   ACDC_Sign=*(reinterpret_cast<int*>(msg.data()));
-  sock->recv(&msg);
-  SELF_Mode=*(reinterpret_cast<int*>(msg.data()));
   sock->recv(&msg);
   SELF_Sign=*(reinterpret_cast<int*>(msg.data()));
   sock->recv(&msg);
@@ -195,6 +185,12 @@ bool Config::Receive(zmq::socket_t* sock){
   Pedestal_channel=*(reinterpret_cast<int*>(msg.data()));
   sock->recv(&msg);
   Pedestal_channel_mask=*(reinterpret_cast<unsigned int*>(msg.data()));
+	
+  //PPS settings
+  sock->recv(&msg);
+  PPSRatio=*(reinterpret_cast<unsigned int*>(msg.data()));
+  sock->recv(&msg);
+  PPSBeamMultiplexer=*(reinterpret_cast<int*>(msg.data()));	
 
   return true;
 }
@@ -242,7 +238,11 @@ bool Config::SetDefaults(){
   //Pedestal set value channel
   Pedestal_channel=3000;
   Pedestal_channel_mask=0x1F;
-
+	
+  //PPS settings
+  PPSRatio = 0x0001;
+  PPSBeamMultiplexer = 1;
+	
   return true;
 }
 
@@ -254,11 +254,8 @@ bool Config::Print(){
   printf("Raw_Mode: %i\n",(int)Raw_Mode);
   std::cout << "------------------Trigger settings------------------" << std::endl;
   printf("Triggermode: %i\n",triggermode);
-  printf("ACC trigger Mode: %i\n", ACC_Mode);
   printf("ACC trigger Sign: %i\n", ACC_Sign);
-  printf("ACDC trigger Mode: %i\n", ACDC_Mode);
   printf("ACDC trigger Sign: %i\n", ACDC_Sign);
-  printf("Selftrigger Mode: %i\n", SELF_Mode);
   printf("Selftrigger Sign: %i\n", SELF_Sign);
   printf("Coincidence Mode: %i\n", SELF_Enable_Coincidence);
   printf("Required Coincidence Channels: %d\n", SELF_Coincidence_Number);
@@ -270,6 +267,9 @@ bool Config::Print(){
   printf("PSEC channelmask (for chip 0 to 4) : 0x%02x|0x%02x|0x%02x|0x%02x|0x%02x\n",PSEC_Channel_Mask_0,PSEC_Channel_Mask_1,PSEC_Channel_Mask_2,PSEC_Channel_Mask_3,PSEC_Channel_Mask_4);
   printf("PSEC pedestal value: %d\n", Pedestal_channel);
   printf("PSEC chipmask for pedestal: 0x%02x\n", Pedestal_channel_mask);
+  std::cout << "------------------PSEC settings------------------" << std::endl;	
+  printf("PPS divider ratio: 0x%04x\n",PPSRatio);
+  printf("PPS multiplexer: %i\n",PPSBeamMultiplexer);
   std::cout << "-------------------------------------------------" << std::endl;
 
 	return true;
