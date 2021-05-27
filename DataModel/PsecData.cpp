d0aa41bae4dafc34e1466d3a705c7f7e0e9f1dc8
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-05-27 16:02:13 +0200

    Update PsecData.cpp

--- DataModel/PsecData.cpp
+++ DataModel/PsecData.cpp
@@ -7,7 +7,7 @@ bool PsecData::Send(zmq::socket_t* sock){
 	int S_RawWaveform = RawWaveform.size();
 	int S_AccInfoFrame = AccInfoFrame.size();
 	int S_errorcodes = errorcodes.size();
-	int S_AcdcInfoFrame = AcdcInfoFrame.size();
+	//int S_AcdcInfoFrame = AcdcInfoFrame.size();
 
 	zmq::message_t msg1(sizeof VersionNumber);
 	std::memcpy(msg1.data(), &VersionNumber, sizeof VersionNumber);
@@ -25,10 +25,10 @@ bool PsecData::Send(zmq::socket_t* sock){
 	zmq::message_t msg6(sizeof(unsigned short) * S_AccInfoFrame);
 	std::memcpy(msg6.data(), AccInfoFrame.data(), sizeof(unsigned short) * S_AccInfoFrame);
 	
-	zmq::message_t msg7(sizeof S_AcdcInfoFrame);
-	std::memcpy(msg7.data(), &S_AcdcInfoFrame, sizeof S_AcdcInfoFrame);
-	zmq::message_t msg8(sizeof(unsigned short) * S_AcdcInfoFrame);
-	std::memcpy(msg8.data(), AcdcInfoFrame.data(), sizeof(unsigned short) * S_AcdcInfoFrame);
+	//zmq::message_t msg7(sizeof S_AcdcInfoFrame);
+	//std::memcpy(msg7.data(), &S_AcdcInfoFrame, sizeof S_AcdcInfoFrame);
+	//zmq::message_t msg8(sizeof(unsigned short) * S_AcdcInfoFrame);
+	//std::memcpy(msg8.data(), AcdcInfoFrame.data(), sizeof(unsigned short) * S_AcdcInfoFrame);
 
 	zmq::message_t msg9(sizeof S_errorcodes);
 	std::memcpy(msg9.data(), &S_errorcodes, sizeof S_errorcodes);
@@ -44,8 +44,8 @@ bool PsecData::Send(zmq::socket_t* sock){
 	sock->send(msg4,ZMQ_SNDMORE);
 	sock->send(msg5,ZMQ_SNDMORE);
 	sock->send(msg6,ZMQ_SNDMORE);
-	sock->send(msg7,ZMQ_SNDMORE);
-	sock->send(msg8,ZMQ_SNDMORE);
+	//sock->send(msg7,ZMQ_SNDMORE);
+	//sock->send(msg8,ZMQ_SNDMORE);
 	sock->send(msg9,ZMQ_SNDMORE);
 	sock->send(msg10,ZMQ_SNDMORE);
 	sock->send(msg11);
@@ -85,15 +85,15 @@ bool PsecData::Receive(zmq::socket_t* sock){
 			std::memcpy(&AccInfoFrame[0], msg.data(), msg.size());
 		}
 		
-		sock->recv(&msg);
-		tmp_size=0;
-		tmp_size=*(reinterpret_cast<int*>(msg.data()));
-		if(tmp_size>0)
-		{
-			sock->recv(&msg);
-			AcdcInfoFrame.resize(msg.size()/sizeof(unsigned short));
-			std::memcpy(&AcdcInfoFrame[0], msg.data(), msg.size());
-		}
+		//sock->recv(&msg);
+		//tmp_size=0;
+		//tmp_size=*(reinterpret_cast<int*>(msg.data()));
+		//if(tmp_size>0)
+		//{
+		//	sock->recv(&msg);
+		//	AcdcInfoFrame.resize(msg.size()/sizeof(unsigned short));
+		//	std::memcpy(&AcdcInfoFrame[0], msg.data(), msg.size());
+		//}
 
 		sock->recv(&msg);
 		tmp_size=0;
@@ -123,7 +123,7 @@ bool PsecData::Print(){
 	printf("Failed read attempts: %i\n", FailedReadCounter);
 	printf("Waveform size: %li\n", RawWaveform.size());
 	printf("ACC Infoframe size: %li\n", AccInfoFrame.size());
-	printf("ACDC Infoframe size: %li\n", AcdcInfoFrame.size());
+	//printf("ACDC Infoframe size: %li\n", AcdcInfoFrame.size());
 	if(errorcodes.size()==1 && errorcodes[0]==0x00000000)
 	{
 		printf("No errorcodes found all good: 0x%08x\n", errorcodes[0]);
