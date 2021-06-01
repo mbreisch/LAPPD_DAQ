4dc2cc78b2601ec0f125769f5872d97f1eb3b2dd
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-06-01 12:13:13 +0200

    Update ACC_Stream.cpp

--- UserTools/ACC_Stream/ACC_Stream.cpp
+++ UserTools/ACC_Stream/ACC_Stream.cpp
@@ -67,6 +67,6 @@ bool ACC_Stream::Finalise(){
 
 unsigned long long getTime()
 {
-	auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(m_clock.now().time_since_epoch()).count()
+	auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock.now().time_since_epoch()).count()
 	return (unsigned long long)time;
 }
