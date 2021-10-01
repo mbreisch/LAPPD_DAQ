085e3a20c2416e292f1946747a63ce262e49afb1
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-10-01 14:02:42 +0200

    Update ACC.cpp

--- DataModel/ACC.cpp
+++ DataModel/ACC.cpp
@@ -317,12 +317,9 @@ int ACC::initializeForDataReadout(int trigMode, unsigned int boardMask, int cali
 			}
 			
 			for(int i=0; i<(int)SELF_psec_chip_mask.size(); i++)
-			{	
-				if(SELF_psec_chip_mask[i]==1)
-				{		
-					command = (command | (boardMask << 24)) | i << 12 | SELF_psec_channel_mask[i]; printf("Mask: 0x%08x\n",command);
-					usbcheck=usb->sendData(command); if(usbcheck==false){writeErrorLog("Send Error");}	
-				}
+			{			
+				command = (command | (boardMask << 24)) | i << 12 | SELF_psec_channel_mask[i]; printf("Mask: 0x%08x\n",command);
+				usbcheck=usb->sendData(command); 	
 			}
 			command = 0x00B16000;
 			command = (command | (boardMask << 24)) | SELF_sign;
