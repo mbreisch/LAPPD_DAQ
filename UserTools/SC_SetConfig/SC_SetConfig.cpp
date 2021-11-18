121b568b3292fecd1048b89c82f450ff7eafcef6
mbreisch <64076923+mbreisch@users.noreply.github.com>
2021-11-18 12:26:33 +0100

    Update SC_SetConfig.cpp

--- UserTools/SC_SetConfig/SC_SetConfig.cpp
+++ UserTools/SC_SetConfig/SC_SetConfig.cpp
@@ -67,9 +67,11 @@ bool SC_SetConfig::Execute(){
     m_data->SCMonitor.relayCh1_mon = m_data->CB->GetRelayState(1);
     m_data->SCMonitor.relayCh2_mon = m_data->CB->GetRelayState(2);
     m_data->SCMonitor.relayCh3_mon = m_data->CB->GetRelayState(3);
+	  
+    m_data->SCMonitor.recieveFlag==1
   }
 	
-  if(m_data->SCMonitor.recieveFlag==0 || m_data->SCMonitor.recieveFlag==1)
+  if(m_data->SCMonitor.recieveFlag==1)
   {
     //------------------------------------Relay Control
     if(m_data->SCMonitor.relayCh1!=m_data->SCMonitor.relayCh1_mon)
