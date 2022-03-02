debb49891bc8650f57d53f214c655d194e1d075e
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-02 12:09:35 +0100

    Update SC_SetConfig.cpp

--- UserTools/SC_SetConfig/SC_SetConfig.cpp
+++ UserTools/SC_SetConfig/SC_SetConfig.cpp
@@ -30,46 +30,7 @@ bool SC_SetConfig::Initialise(std::string configfile, DataModel &data){
 bool SC_SetConfig::Execute(){
   int retval;
   //check LV/HV state_set 
-  if(m_data->SCMonitor.recieveFlag==0)
-  {
-    bool temp_HVS;
-    m_variables.Get("HV_state_set",temp_HVS);
-    m_data->SCMonitor.HV_state_set = temp_HVS;
-
-    bool temp_LVS;
-    m_variables.Get("LV_state_set",temp_LVS);
-    m_data->SCMonitor.LV_state_set = temp_LVS;
-
-    float temp_HVV;
-    m_variables.Get("HV_volts",temp_HVV);
-    m_data->SCMonitor.HV_volts = temp_HVV;
-
-    float temp_Tr1;
-    m_variables.Get("Trig1_threshold",temp_Tr1);
-    m_data->SCMonitor.Trig1_threshold = temp_Tr1;
-
-    float temp_Tr0;
-    m_variables.Get("Trig0_threshold",temp_Tr0);
-    m_data->SCMonitor.Trig0_threshold = temp_Tr0;
-
-    float temp_VREF;
-    m_variables.Get("TrigVref",temp_VREF);
-    m_data->SCMonitor.TrigVref = temp_VREF;
-
-    bool temp_r1, temp_r2,temp_r3;
-    m_variables.Get("relayCh1",temp_r1);
-    m_data->SCMonitor.relayCh1 = temp_r1;
-    m_variables.Get("relayCh2",temp_r2);
-    m_data->SCMonitor.relayCh2 = temp_r2;
-    m_variables.Get("relayCh3",temp_r3);
-    m_data->SCMonitor.relayCh3 = temp_r3;
-    
-    m_data->SCMonitor.relayCh1_mon = m_data->CB->GetRelayState(1);
-    m_data->SCMonitor.relayCh2_mon = m_data->CB->GetRelayState(2);
-    m_data->SCMonitor.relayCh3_mon = m_data->CB->GetRelayState(3);
-	  
-    m_data->SCMonitor.recieveFlag==1;
-  }
+  if(m_data->SCMonitor.recieveFlag==0){return true;}
 	
   if(m_data->SCMonitor.recieveFlag==1)
   {
