d63588b610cc46661749dd021c1b9acc065bddff
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-07 12:21:21 +0100

    Update SC_SetConfig.cpp

--- UserTools/SC_SetConfig/SC_SetConfig.cpp
+++ UserTools/SC_SetConfig/SC_SetConfig.cpp
@@ -5,25 +5,25 @@ SC_SetConfig::SC_SetConfig():Tool(){}
 
 bool SC_SetConfig::Initialise(std::string configfile, DataModel &data){
 
-  if(configfile!="")  m_variables.Initialise(configfile);
-  //m_variables.Print();
+	if(configfile!="")  m_variables.Initialise(configfile);
+	//m_variables.Print();
 
-  m_data= &data;
-  m_log= m_data->Log;
- 
-  m_data->CB= new Canbus();
-  m_data->CB->Connect(); 
-	
-  std::fstream infile("./configfiles/SlowControl/LastHV.txt", std::ios_base::in);
-  if(infile.is_open())
-  {
-  	infile >> m_data->CB->get_HV_volts;
-  	infile.close();
-  }
-	
-  if(!m_variables.Get("verbose",m_verbose)) m_verbose=1;
+	m_data= &data;
+	m_log= m_data->Log;
 
-  return true;
+	m_data->CB= new Canbus();
+	m_data->CB->Connect(); 
+
+	std::fstream infile("./configfiles/SlowControl/LastHV.txt", std::ios_base::in);
+	if(infile.is_open())
+	{
+		infile >> m_data->CB->get_HV_volts;
+		infile.close();
+	}
+
+	if(!m_variables.Get("verbose",m_verbose)) m_verbose=1;
+
+	return true;
 }
 
 
@@ -40,7 +40,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetRelay(1,m_data->SCMonitor.relayCh1);
 			if(retval!=0 && retval!=1)
 			{
-				std::cout << " There was an error (Relay 1) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (Relay 1) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB01EE01);
 			}
 		}
 
@@ -49,7 +50,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetRelay(2,m_data->SCMonitor.relayCh2);
 			if(retval!=0 && retval!=1)
 			{
-				std::cout << " There was an error (Relay 2) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (Relay 2) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB01EE02);
 			}
 		}
 
@@ -58,7 +60,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetRelay(3,m_data->SCMonitor.relayCh3);
 			if(retval!=0 && retval!=1)
 			{
-				std::cout << " There was an error (Relay 3) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (Relay 3) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB01EE03);
 			}
 		}  
     
@@ -66,7 +69,8 @@ bool SC_SetConfig::Execute(){
 		retval = m_data->CB->SetLV(m_data->SCMonitor.LV_state_set);
 		if(retval!=0 && retval!=1)
 		{
-			std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
+			//std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
+			m_data->SCMonitor.errorcodes.push_back(0xCB02EE01);
 		}
     		//------------------------------------HV Control
 		bool tempHVmon;
@@ -85,16 +89,18 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetHV_ONOFF(m_data->SCMonitor.HV_state_set);
 			if(retval!=0 && retval!=1)
 			{
-				std::cout << " There was an error (Set HV) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (Set HV) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB03EE01);
 			}
     		}
 		
 		int retstate = m_data->CB->GetHV_ONOFF();
 		m_data->SCMonitor.HV_return_mon = m_data->CB->ReturnedHvValue;
-		if(abs(m_data->CB->get_HV_volts-m_data->SCMonitor.HV_return_mon)>0.1)
+		if(abs(m_data->CB->get_HV_volts-m_data->SCMonitor.HV_return_mon)>0.5)
 		{
-			std::cout << "ERROR! " << "File gave " << m_data->CB->get_HV_volts << " Readback gave " << m_data->SCMonitor.HV_return_mon << std::endl;
-			std::cout << "Setting them as the read back value" << std::endl;
+			//std::cout << "ERROR! " << "File gave " << m_data->CB->get_HV_volts << " Readback gave " << m_data->SCMonitor.HV_return_mon << std::endl;
+			//std::cout << "Setting them as the read back value" << std::endl;
+			m_data->SCMonitor.errorcodes.push_back(0xCB03EE02);
 			m_data->CB->get_HV_volts = m_data->SCMonitor.HV_return_mon;
 		}
 
@@ -109,7 +115,8 @@ bool SC_SetConfig::Execute(){
 				outfile.close();
 			}else
 			{
-				std::cout << " There was an error (HV V set) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (HV V set) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB03EE03);
 			}
 		}
 	  
@@ -130,7 +137,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetLV(m_data->SCMonitor.LV_state_set);
 			if(retval!=0 && retval!=1)
 			{
-				std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB04EE01);
 			}
 		}
 
@@ -142,7 +150,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetTriggerDac0(m_data->SCMonitor.Trig0_threshold, m_data->SCMonitor.TrigVref);
 			if(retval!=0)
 			{
-				std::cout << " There was an error (DAC0) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (DAC0) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB05EE01);
 			}
 			tempval = m_data->CB->GetTriggerDac0(m_data->SCMonitor.TrigVref);
 			if(std::abs(tempval - m_data->SCMonitor.Trig0_threshold)<0.001)
@@ -150,7 +159,8 @@ bool SC_SetConfig::Execute(){
 				m_data->SCMonitor.Trig0_mon = tempval;
 			}else
 			{
-				std::cout << " There was an error (DAC0) - 0xC0 hasn't been updated!" << std::endl;
+				//std::cout << " There was an error (DAC0) - 0xC0 hasn't been updated!" << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB05EE02);
 			}
 		}
 
@@ -159,7 +169,8 @@ bool SC_SetConfig::Execute(){
 			retval = m_data->CB->SetTriggerDac1(m_data->SCMonitor.Trig1_threshold, m_data->SCMonitor.TrigVref);
 			if(retval!=0)
 			{
-				std::cout << " There was an error (DAC1) with retval: " << retval << std::endl;
+				//std::cout << " There was an error (DAC1) with retval: " << retval << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB05EE03);
 			}
 			tempval = m_data->CB->GetTriggerDac1(m_data->SCMonitor.TrigVref);
 			if(std::abs(tempval - m_data->SCMonitor.Trig1_threshold)<0.001)
@@ -167,7 +178,8 @@ bool SC_SetConfig::Execute(){
 				m_data->SCMonitor.Trig1_mon = tempval;
 			}else
 			{
-				std::cout << " There was an error (DAC1) - 0xC0 hasn't been updated!" << std::endl;
+				//std::cout << " There was an error (DAC1) - 0xC0 hasn't been updated!" << std::endl;
+				m_data->SCMonitor.errorcodes.push_back(0xCB05EE04);
 			}
 		} 
     
