ef0e833769e585f5c735ff3f327397f3856fedaa
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-16 13:04:11 +0100

    Update SC_SetConfig.cpp

--- UserTools/SC_SetConfig/SC_SetConfig.cpp
+++ UserTools/SC_SetConfig/SC_SetConfig.cpp
@@ -88,7 +88,7 @@ bool SC_SetConfig::Setup(){
 	}  
 
 	//------------------------------------HV Prep
-	retval = m_data->CB->SetLV(0);
+	retval = m_data->CB->SetLV(false);
 	if(retval!=0 && retval!=1)
 	{
 		//std::cout << " There was an error (Set LV) with retval: " << retval << std::endl;
