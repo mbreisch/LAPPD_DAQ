3ad714338e259ec68d5262174eecce4b4274f36d
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-18 16:12:31 +0100

    Update SC_SetConfig.cpp

--- UserTools/SC_SetConfig/SC_SetConfig.cpp
+++ UserTools/SC_SetConfig/SC_SetConfig.cpp
@@ -20,6 +20,10 @@ bool SC_SetConfig::Initialise(std::string configfile, DataModel &data){
 		infile >> m_data->CB->get_HV_volts;
 		infile.close();
 	}
+	
+	string ThermistorID;
+	m_variables.Get("ThermistorID",ThermistorID)
+	m_data->CB.thermistorID = ThermistorID;
 
 	if(!m_variables.Get("verbose",m_verbose)) m_verbose=1;
 
