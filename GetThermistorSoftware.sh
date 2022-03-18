fbb3672d1e0586805b90c4bd77e068e1fbf353cd
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-03-18 16:13:35 +0100

    Update GetThermistorSoftware.sh

--- GetThermistorSoftware.sh
+++ GetThermistorSoftware.sh
@@ -3,10 +3,9 @@
 mkdir -p ToolDAQ
 cd ToolDAQ #into ToolDAQ
 
-mkdir YOCTO 
 git clone https://github.com/yoctopuce/yoctolib_cpp.git YOCTO
 cd YOCTO #into YOCTO
-./build.sh
-cd Binaries/linux/aarch64
+./build.sh armhf
+cd Binaries/linux/armhf
 ln -s libyocto.so.1.0.1 libyocto.so
 cd ../../../../.. #into Main folder
