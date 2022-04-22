2d75ace372245008672ba1b9e522036be48a5254
mbreisch <64076923+mbreisch@users.noreply.github.com>
2022-04-22 16:21:18 +0200

    Patch DAQ 8

--- DataModel/TempClassStore.h
+++ DataModel/TempClassStore.h
@@ -21,15 +21,10 @@ class TempClassStore{
  private:
 
     //For Fake data use
-    /*
-    int help1=0;
-    int help2=0;
-    int counter=0;
-
     map<int,vector<unsigned short>> ReadFileData;
     vector<unsigned short> ReadFileACC;
     vector<unsigned int> ReadFileError;
-    */
+
 };
 
 #endif
