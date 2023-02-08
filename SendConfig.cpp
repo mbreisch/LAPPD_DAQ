0304984075042761d509a8302fa7ae95e69ec26f
mbreisch <64076923+mbreisch@users.noreply.github.com>
2023-02-08 20:28:13 +0100

    Update SendConfig.cpp

--- SendConfig.cpp
+++ SendConfig.cpp
@@ -16,9 +16,10 @@ int main(){
     std::cout<<"NOTICE!!!!"<<std::endl;
     std::cout<<"Recent changes: "<<std::endl;
     std::cout<<"- Humidity is now a temperature dependent limit. You can keep 15 as lower limit and 20 as upper!"<<std::endl;
+    std::cout<<"-Saltbridge emergency limit is now 200000 in all presets!"<<std::endl;
 
-    std::cout<<"Please enter the LAPPD ID number (Please check beforehand since the monitoring needs it!): ";
-    std::cin>>data.LAPPD_ID;  
+    //std::cout<<"Please enter the LAPPD ID number (Please check beforehand since the monitoring needs it!): ";
+    //std::cin>>data.LAPPD_ID;  
 
     int default_load = 0;
     std::cout<<"Do you want to load default settings (0: no |1: yes): ";
